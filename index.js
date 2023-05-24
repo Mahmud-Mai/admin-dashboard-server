35f4eec367d9b6324267de99acdef8d06ba620f0
Mahmud Mai <mahmudx00@gmail.com>
2023-05-24 10:28:56 +0100

    Feat: create MVC for transactions

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import dotenv from 'dotenv'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
 /* One time bulk data imports */
+import Transaction from "./models/Transaction.js";
 // import User from "./models/User.js"
 // import Product from './models/Product.js'
 // import ProductStat from './models/ProductStat.js'
-// import { dataUser, dataProduct, dataProductStat } from './data/index.js' 
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from "./data/index.js";
 
 /* CONFIGURATION */
-dotenv.config()
-const app = express()
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin'}))
-app.use(morgan('common'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors)
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors);
 
 /* ROUTES */
-app.use('/general', generalRoutes)
-app.use('/client', clientRoutes)
-app.use('/management', managementRoutes) 
-app.use('/sales', salesRoutes)
+app.use("/general", generalRoutes);
+app.use("/client", clientRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
 
 /* MONGOOSE SETUP*/
 
@@ -42,12 +48,14 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Connection Successful. \nServer Port: ${PORT}`));
-        
-        /* Only Add data Once  */
-        // User.insertMany(dataUser);
-        // Product.insertMany(dataProduct)
-        // ProductStat.insertMany(dataProductStat)
+    app.listen(PORT, () =>
+      console.log(`Connection Successful. \nServer Port: ${PORT}`)
+    );
 
-    })
-    .catch(error => console.log(`${error} did not connect to DB`))
+    /* Only Add data Once  */
+    // Transaction.insertMany(dataTransaction)
+    // User.insertMany(dataUser);
+    // Product.insertMany(dataProduct)
+    // ProductStat.insertMany(dataProductStat)
+  })
+  .catch((error) => console.log(`${error} did not connect to DB`));
